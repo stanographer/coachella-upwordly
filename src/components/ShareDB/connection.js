5188df736e08cc0feb637b8771da2d87185fadfc
Stanley Sakai <stanley@stanographer.com>
2020-03-01 14:05:13 -0500

    Changed default to production websocket address.

--- src/components/ShareDB/connection.js
+++ src/components/ShareDB/connection.js
@@ -3,11 +3,11 @@ import ReconnectingWebSocket from 'reconnecting-websocket';
 import otText from 'ot-text';
 
 const server = {
-  host: process.env.REACT_APP_ENV === 'production' ? 'upword.ly/ws' : window.location.hostname,
-  port: process.env.REACT_APP_ENV === 'production' ? '' : ':9090',
-  protocol: process.env.REACT_APP_ENV === 'production' ? 'wss://' : 'ws://',
+  host: 'upword.ly/ws',
+  protocol: 'wss://',
+
   getAddress: function() {
-    return this.protocol + this.host + this.port;
+    return this.protocol + this.host;
   },
 };
 
